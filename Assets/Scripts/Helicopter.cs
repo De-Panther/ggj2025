348b81a2679c273d40fd7a6b891b269e91202fdc
De-Panther <depanther@gmail.com>
2025-02-01 02:06:22 +0200

    Better Helicopter controls

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -16,10 +16,14 @@ namespace GGJGame
     [SerializeField]
     private Rigidbody _Rigidbody;
 
-    private float heightSpeed = 7f;
-    private float sidesSpeed = 7f;
-    private float forwardSpeed = 10f;
+    private float heightSpeed = 14f;
+    private float sidesSpeed = 14f;
+    private float forwardSpeed = 20f;
     private float rotationSpeed = 45f;
+    private float propellerSpeed = 900f;
+
+    private Vector2 leftStick = Vector2.zero;
+    private Vector2 rightStick = Vector2.zero;
 
     private void OnEnable()
     {
@@ -35,50 +39,59 @@ namespace GGJGame
 
     private void Update()
     {
-      propeller.Rotate(0, 90*Time.deltaTime, 0, Space.Self);
+      propeller.Rotate(0, propellerSpeed * Time.deltaTime * (1 + Mathf.Abs(leftStick.x) + Mathf.Abs(leftStick.y) + Mathf.Abs(rightStick.x) + Mathf.Clamp(rightStick.y, -0.25f, 1f)), 0, Space.Self);
       if (!coherenceSync.HasStateAuthority)
       {
         return;
       }
+      Keyboard keyboard = Keyboard.current;
       Gamepad gamepad = Gamepad.current;
+      if (keyboard != null)
+      {
+        leftStick.x = keyboard.aKey.isPressed ? -1 : 0;
+        leftStick.x += keyboard.dKey.isPressed ? 1 : 0;
+        leftStick.y = keyboard.wKey.isPressed ? 1 : 0;
+        leftStick.y += keyboard.sKey.isPressed ? -1 : 0;
+        rightStick.x = keyboard.jKey.isPressed ? -1 : 0;
+        rightStick.x += keyboard.lKey.isPressed ? 1 : 0;
+        rightStick.y = keyboard.iKey.isPressed ? 1 : 0;
+        rightStick.y += keyboard.kKey.isPressed ? -1 : 0;
+      }
+      else
+      {
+        leftStick = Vector2.zero;
+        rightStick = Vector2.zero;
+      }
       if (gamepad == null)
       {
         return;
       }
-      Vector2 leftStick = gamepad.leftStick.ReadValue(); // Forward and Rotation
-      Vector2 rightStick = gamepad.rightStick.ReadValue(); // Height and Sides
-
-      transform.Rotate(0, leftStick.x * rotationSpeed * Time.deltaTime, 0, Space.Self);
-      transform.Translate(rightStick.x * sidesSpeed * Time.deltaTime, // Sides
-        rightStick.y * heightSpeed * Time.deltaTime, // Height
-        leftStick.y * forwardSpeed * Time.deltaTime, // Forward
-        Space.Self);
+      leftStick += gamepad.leftStick.ReadValue(); // Forward and Rotation
+      rightStick += gamepad.rightStick.ReadValue(); // Height and Sides
+      leftStick.x = Mathf.Clamp(leftStick.x, -1f, 1f);
+      leftStick.y = Mathf.Clamp(leftStick.y, -1f, 1f);
+      rightStick.x = Mathf.Clamp(rightStick.x, -1f, 1f);
+      rightStick.y = Mathf.Clamp(rightStick.y, -1f, 1f);
     }
 
-    private void FixedUpdate1()
+    private void FixedUpdate()
     {
       if (!coherenceSync.HasStateAuthority)
       {
         return;
       }
-      Gamepad gamepad = Gamepad.current;
-      Vector2 leftStick = gamepad.leftStick.ReadValue(); // Forward and Rotation
-      Vector2 rightStick = gamepad.rightStick.ReadValue(); // Height and Sides
 
-      // transform.Rotate(0, leftStick.x * rotationSpeed * Time.deltaTime, 0, Space.Self);
-      // transform.Translate(rightStick.x * sidesSpeed * Time.deltaTime, // Sides
-      //   rightStick.y * heightSpeed * Time.deltaTime, // Height
-      //   leftStick.y * forwardSpeed * Time.deltaTime, // Forward
-      //   Space.Self);
-      // _Rigidbody.AddTorque(0, leftStick.x * rotationSpeed * Time.deltaTime, 0, ForceMode.VelocityChange);
-      // _Rigidbody.AddForce(rightStick.x * sidesSpeed * Time.deltaTime, // Sides
-      //   rightStick.y * heightSpeed * Time.deltaTime, // Height
-      //   leftStick.y * forwardSpeed * Time.deltaTime, // Forward
-      //   ForceMode.VelocityChange);
-      // _Rigidbody.linearVelocity = new Vector3(rightStick.x * sidesSpeed, // Sides
-      //   rightStick.y * heightSpeed, // Height
-      //   leftStick.y * forwardSpeed); // Forward
-      // _Rigidbody.angularVelocity = new Vector3(0, leftStick.x * rotationSpeed * Mathf.Deg2Rad, 0); // Forward
+      Vector3 targetAngularVelocity = new Vector3(0, leftStick.x * rotationSpeed * Mathf.Deg2Rad, 0);
+      Vector3 targetVelocity = new Vector3(rightStick.x * sidesSpeed, // Sides
+        rightStick.y * heightSpeed, // Height
+        leftStick.y * forwardSpeed); // Forward
+      _Rigidbody.angularVelocity = Vector3.zero;
+      _Rigidbody.linearVelocity = Vector3.zero;
+      _Rigidbody.AddRelativeTorque(targetAngularVelocity, ForceMode.VelocityChange);
+      _Rigidbody.AddRelativeForce(targetVelocity, ForceMode.VelocityChange);
+      Quaternion currentRotation = _Rigidbody.rotation;
+      Quaternion targetRotation = Quaternion.Euler(0, currentRotation.eulerAngles.y, 0);
+      _Rigidbody.MoveRotation(Quaternion.RotateTowards(currentRotation, targetRotation, rotationSpeed * 0.1f));
     }
   }
 }
